7633a6f5952f7b824fec719670f3f9cec8d2bcf3
Kovandowski <matteo.cetraro@icloud.com>
2026-05-04 09:35:38 +0200

    #7 Implementazione dei Test per 1-100

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -32,101 +32,120 @@ public class IntegerToRomanTest {
 
     @Test 
     public void convertTestVII(){
-        //Arrange
         int input=7;
-        //Act
         String t = IntegerToRoman.convert(input);
-        //Assert
         assertEquals("VII", t);
     }
 
      @Test 
     public void convertTestIX(){
-        //Arrange
         int input=9;
-        //Act
         String t = IntegerToRoman.convert(input);
-        //Assert
         assertEquals("IX", t);
     }
 
     @Test 
     public void convertTestX(){
-        //Arrange
         int input=10;
-        //Act
         String t = IntegerToRoman.convert(input);
-        //Assert
         assertEquals("X", t);
     }
 
     @Test 
     public void convertTestXIV(){
-        //Arrange
         int input=14;
-        //Act
         String t = IntegerToRoman.convert(input);
-        //Assert
         assertEquals("XIV", t);
     }
 
      @Test 
     public void convertTestXIX(){
-        //Arrange
         int input=19;
-        //Act
         String t = IntegerToRoman.convert(input);
-        //Assert
         assertEquals("XIX", t);
     }
 
      @Test 
     public void convertTestXX(){
-        //Arrange
         int input=20;
-        //Act
         String t = IntegerToRoman.convert(input);
-        //Assert
         assertEquals("XX", t);
     }
 
     @Test 
     public void convertTestXL(){
-        //Arrange
         int input=40;
-        //Act
         String t = IntegerToRoman.convert(input);
-        //Assert
         assertEquals("XL", t);
     }
 
     @Test 
     public void convertTestXLIV(){
-        //Arrange
         int input=44;
-        //Act
         String t = IntegerToRoman.convert(input);
-        //Assert
         assertEquals("XLIV", t);
     }
 
      @Test 
     public void convertTestXLIX(){
-        //Arrange
         int input=49;
-        //Act
         String t = IntegerToRoman.convert(input);
-        //Assert
         assertEquals("XLIX", t);
     }
 
      @Test 
     public void convertTestL(){
-        //Arrange
         int input=50;
-        //Act
         String t = IntegerToRoman.convert(input);
-        //Assert
         assertEquals("L", t);
+    }
+
+    @Test
+    public void convertTestLX(){
+        int input = 60;
+        String t = IntegerToRoman.convert(input);
+        assertEquals("LX",t);
+    }
+
+    @Test
+    public void convertTestLXIV(){
+        int input = 64;
+        String t = IntegerToRoman.convert(input);
+        assertEquals("LXIV",t);
+    }
+
+    @Test
+    public void convertTestLXXIX(){
+        int input = 79;
+        String t = IntegerToRoman.convert(input);
+        assertEquals("LXXIX",t);
+    }
+
+    @Test
+    public void convertTestLXXXIV(){
+        int input = 84;
+        String t = IntegerToRoman.convert(input);
+        assertEquals("LXXXIV",t);
+    }
+
+    @Test
+    public void convertTestCX(){
+        int input = 90;
+        String t = IntegerToRoman.convert(input);
+        assertEquals("CX",t);
+    }
+
+    @Test
+    public void convertTestCXIV(){
+        int input = 94;
+        String t = IntegerToRoman.convert(input);
+        assertEquals("CXIV",t);
+    }
+
+    @Test
+    public void convertTestCXIX(){
+        int input = 99;
+        String t = IntegerToRoman.convert(input);
+        assertEquals("CXIX",t);
     }
 }
