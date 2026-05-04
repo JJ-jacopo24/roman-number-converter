5fa45a13f9eb4f5cb7329ba07917c510f55f9fb6
Kovandowski <matteo.cetraro@icloud.com>
2026-05-04 18:46:55 +0200

    #41 Implementazione test per stampa del carattere L

--- roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -118,4 +118,56 @@ public class RomanPrinterTest {
         //assert
         assertEquals(expected, result);
     }
+
+    @Test
+    public void printTest50(){
+        //arrange
+        String expected =
+        " _      \n" +
+        "| |     \n" +
+        "| |     \n" +
+        "| |     \n" +
+        "| |____ \n" +
+        "|______|\n";
+
+        //act
+        String result = RomanPrinter.print(50);
+        System.out.println(RomanPrinter.print(50));
+        //assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void printTest56(){
+        //arrange
+        String L =
+        " _      \n" +
+        "| |     \n" +
+        "| |     \n" +
+        "| |     \n" +
+        "| |____ \n" +
+        "|______|\n";
+        String V =
+        "__      __\n" +
+        "\\ \\    / /\n" +
+        " \\ \\  / / \n" +
+        "  \\ \\/ /  \n" +
+        "   \\  /   \n" +
+        "    \\/    \n";
+        String I =
+        " _____ \n" +
+        "|_   _|\n" +
+        "  | |  \n" +
+        "  | |  \n" +
+        " _| |_ \n" +
+        "|_____|\n";
+
+        String expected = L + V + I;
+
+        //act
+        String result = RomanPrinter.print(56);
+        System.out.println(RomanPrinter.print(56));
+        //assert
+        assertEquals(expected, result);
+    }
 }
